23c96c805e065ffd120728b4640483b0763317b0
Rijo Sebastian <rijobdk@gmail.com>
2023-10-22 21:02:02 +0530

    Implement JavaScript for dark/light theme toggler and mobile navigation toggler

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,15 @@
+"use strict";
+
 /**
- * @copyright codewithsadee 2023
+ * Attaches an event listener to a collection of DOM elements
+ * @param {Array<HTMLElement} $elements - An array of Dom elements to attach the even listener to
+ * @param {string} eventType - The type of event to listen for (e.g. click, mouseover, etc.)
+ * @param {Function} callback - The callback function to execute when the event is occurs
  */
 
-'use strict';
+const addEventOnElements = function ($elements, eventType, callback) {
+  $elements.forEach(($element) =>
+    $element.addEventListener(eventType, callback));
+};
+
+export { addEventOnElements };

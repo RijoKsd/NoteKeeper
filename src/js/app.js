ec4b84134fbffe393b784235cf35ba83d95177e1
Rijo Sebastian <rijobdk@gmail.com>
2023-10-23 17:24:19 +0530

    Implement functionality to display a notebook creation field on clicking the "Notebooks plus symbol" button
    - This commit introduces a feature that displays a notebook creation field within the sidebar upon the user's click on the "Notebooks plus symbol" button.
    -  Additionally, the newly created notebook is stored in the browser's local storage, ensuring that the user's data is persistently saved for future access.

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,15 @@
 /**
  * Module import
  */
-import { addEventOnElements, getGreetingMsg } from "./utils.js";
-import { Tooltip} from "./components/Tooltip.js"
+import {
+  addEventOnElements,
+  getGreetingMsg,
+  activeNotebook,
+  makeElemEditable,
+} from "./utils.js";
+import { Tooltip } from "./components/Tooltip.js";
+import { db } from "./db.js";
+import { client } from "./client.js";
 
 /**
  * Toggle sidebar in small screen
@@ -27,9 +34,9 @@ addEventOnElements($sidebarTogglers, "click", function () {
  * Initialize tootip behavior for all DOM elements with [data-tooltip] attribute.
  */
 
-const /** {Array<HTMLElement} */ $tooltipElems = document.querySelectorAll("[data-tooltip]");
-$tooltipElems.forEach($elem => Tooltip($elem));
-
+const /** {Array<HTMLElement} */ $tooltipElems =
+    document.querySelectorAll("[data-tooltip]");
+$tooltipElems.forEach(($elem) => Tooltip($elem));
 
 /**
  * Show greeting message on homepage
@@ -46,8 +53,10 @@ $greetElem.textContent = getGreetingMsg(currentHour);
  * Show current date on homepage
  */
 
-const /** {HTMLElement} */ $currentDateElem = document.querySelector("[data-current-date]");
-$currentDateElem.textContent = new Date().toDateString().replace(' ',', ');
+const /** {HTMLElement} */ $currentDateElem = document.querySelector(
+    "[data-current-date]"
+  );
+$currentDateElem.textContent = new Date().toDateString().replace(" ", ", ");
 // $currentDateElem.textContent = new Date().toLocaleDateString("en-US", {
 //   weekday: "long",
 //   year: "numeric",
@@ -55,4 +64,63 @@ $currentDateElem.textContent = new Date().toDateString().replace(' ',', ');
 //   day: "numeric",
 // });
 
+/**
+ * Notebook create field
+ */
 
+const /** {HTMLElement } */ $sidebarList = document.querySelector(
+    "[data-sidebar-list]"
+  );
+const /** {HTMLElement} */ $addNotebookBtn = document.querySelector(
+    "[data-add-notebook]"
+  );
+
+/**
+ * Shows a notebook creation field in the sidebar when the "Add Notebook" button is clicked.
+ * The function dynamically adds a new notebook field element, makes it editable, and listens for the "Enter" key to create a new notebook.
+ */
+
+const showNotebookField = function () {
+  const /** {HTMLElement } */ $navItem = document.createElement("div");
+  $navItem.classList.add("nav-item");
+  $navItem.innerHTML = `
+        <span class="text text-label-large" data-notebook-field></span>
+        <div class="state-layer"></div>
+    `;
+  $sidebarList.appendChild($navItem);
+  const /** {HTMLElement} */ $navItemField = $navItem.querySelector(
+      "[data-notebook-field]"
+    );
+
+  // Active new created notebook and deactivate the previous one
+  activeNotebook.call($navItem);
+
+  //  Make notebook field content editable and focus
+  makeElemEditable($navItemField);
+
+  // when user press 'Enter' then create notebook
+  $navItemField.addEventListener("keydown", createNotebook);
+};
+
+$addNotebookBtn.addEventListener("click", showNotebookField);
+
+/**
+ * Create new notebook
+ * Creates a new notebook when the 'Enter' key is pressed while editing a notebook name field
+ * The new notebook is stored in the browser's local storage
+ * @param {KeyboardEvent} event - The keyboard event that triggered notebook creation
+ */
+
+const createNotebook = function (event) {
+  if (event.key === "Enter") {
+    // Store new created notebook in local storage
+    const /**{Object}*/ notebookData = db.post.notebook(
+        this.textContent || "Untitled"
+      ); //this: $navItemField
+    this.parentElement.remove();
+
+    // Render navItem
+
+    client.notebook.create(notebookData);
+  }
+};
--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,32 @@
-/**
- * @copyright codewithsadee 2023
- */
+"use strict";
 
-'use strict';
+/**
+ * Import module
+ */
+import { NavItem } from "./components/NavItem.js";
+import { activeNotebook } from "./utils.js";
+
+const /**{HTMLElement} */ $sidebarList = document.querySelector("[data-sidebar-list]");
+
+/**
+ * The client object manages interactions with the user interface (UI) to create, read, update and delete notebooks and notes
+ * It provides functions for performing these operations and updating the UI accordingly
+ * @namespace
+ * @property {Object} notebook - Functions for managing notebooks
+ * @property {Object} note - Functions for managing notes in the UI
+ */
+export const client = {
+  notebook: {
+    /**
+         * Creates a new notebook in the UI, based on provided notebook data 
+         * 
+         * @param {Object} notebookData - Data representing the new notebook
+       
+         */
+    create(notebookData) {
+      const /**{HTMLElement} */ $navItem = NavItem(notebookData.id, notebookData.name);
+        $sidebarList.appendChild($navItem);
+        activeNotebook.call($navItem);
+    },
+  },
+};

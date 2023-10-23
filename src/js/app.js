d7cd58732f3375a53038134254b2b01f9b2ba7dc
Rijo Sebastian <rijobdk@gmail.com>
2023-10-23 12:09:00 +0530

    Initilaze greeting and current time in header section using javascript - This commit introduces functionality that initializes a welcoming message and dynamically updates the current time in the header section of the application. -  This feature enhances user interaction by providing a personalized greeting and real-time display of the current time.

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,49 @@
 /**
  * Module import
  */
-import { addEventOnElements } from "./utils.js"; 
+import { addEventOnElements, getGreetingMsg } from "./utils.js";
 
 /**
  * Toggle sidebar in small screen
  */
 
 const /** {HTMLElement} */ $sidebar = document.querySelector("[data-sidebar]");
-const /**{Array<HTMLElement>} */ $sidebarTogglers = document.querySelectorAll ("[data-sidebar-toggler]");
-const /**{HTMLElement} */ $overlay = document.querySelector("[data-sidebar-overlay]");
+const /**{Array<HTMLElement>} */ $sidebarTogglers = document.querySelectorAll(
+    "[data-sidebar-toggler]"
+  );
+const /**{HTMLElement} */ $overlay = document.querySelector(
+    "[data-sidebar-overlay]"
+  );
 
-addEventOnElements($sidebarTogglers, "click", function(){
-    $sidebar.classList.toggle("active");
-    $overlay.classList.toggle("active");
+addEventOnElements($sidebarTogglers, "click", function () {
+  $sidebar.classList.toggle("active");
+  $overlay.classList.toggle("active");
 });
+
+/**
+ * Show greeting message on homepage
+ */
+
+const /** {HTMLElement } */ $greetElem =
+    document.querySelector("[data-greeting]");
+
+const /** {number} */ currentHour = new Date().getHours();
+
+$greetElem.textContent = getGreetingMsg(currentHour);
+
+/**
+ * Show current date on homepage
+ */
+
+const /** {HTMLElement} */ $currentDateElem = document.querySelector("[data-current-date]");
+console.log("🚀 ~ file: app.js:41 ~ currentDateElem:", $currentDateElem);
+
+$currentDateElem.textContent = new Date().toDateString().replace(' ',', ');
+// $currentDateElem.textContent = new Date().toLocaleDateString("en-US", {
+//   weekday: "long",
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+// });
+
+
--- src/js/utils.js
+++ src/js/utils.js
@@ -9,7 +9,34 @@
 
 const addEventOnElements = function ($elements, eventType, callback) {
   $elements.forEach(($element) =>
-    $element.addEventListener(eventType, callback));
+    $element.addEventListener(eventType, callback)
+  );
 };
 
-export { addEventOnElements };
+/**
+ * Generates a greeting message based on the current hour of the day
+ *
+ * @param {number} currentHour - The current hour of the day (0-23)
+ * @returns {string} - A greeting message with a salutation based on the current hour
+ */
+
+const getGreetingMsg = function (currentHour) {
+  const /** {string} */  greeting = 
+  currentHour < 5 ? "Night" : 
+  currentHour < 12 ? "Morning" :
+  currentHour < 18 ? "Afternoon" :
+  "evening";
+
+  /* 
+  currentHour < 5 ? "Night" :
+  currentHour < 12 ? "Morning" :
+  currentHour < 15 ? "Noon":
+  currentHour < 17 ? "Afternoon" :
+  currentHour < 20 ? "Evening" :
+  "Night";
+  */
+ 
+  return `Good ${greeting}`;
+};
+ 
+export { addEventOnElements, getGreetingMsg };
